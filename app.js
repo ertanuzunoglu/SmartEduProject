a9101b4ca1633bc6c4b0a6bcdbfeca7059ef29d2
ertanuzunoglu <ertanuzunogl@gmail.com>
2023-02-21 15:12:57 +0300

    Lesson 02 : static files updated

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/', (req, res) => res.status(200).send('index sayfasi'));
-app.get('/contact', (req, res) => res.status(200).send('contact sayfasi'));
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.status(200).render('index', { pageName: 'index' }));
+app.get('/about', (req, res) => res.status(200).render('about', { pageName: 'about' }));
 const port = 3000;
 app.listen(port, () => console.log(`App started on port ${port}`));
