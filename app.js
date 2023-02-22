a10879497a646c08855854e4ad1c597b04e0158c
ertanuzunoglu <ertanuzunogl@gmail.com>
2023-02-22 16:10:07 +0300

    Lesson 03 : single-page created

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//db connect
+mongoose.set('strictQuery', false);
+mongoose
+    .connect('mongodb://localhost/smartedu-db')
+    .then(console.log('db connection successful'))
+    .catch(err => console.log('db connection failed'));
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => console.log(`App started on port ${port}`));
---
+++ controllers/courseController.js
@@ -0,0 +1,46 @@
+const Course = require('../models/Course');
+
+exports.createCourse = async (req, res) => {
+    try {
+        const course = await Course.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            course: course,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
+
+exports.getAllCourses = async (req, res) => {
+    try {
+        const courses = await Course.find().sort('-createdAt');
+        res.status(200).render('courses', {
+            courses,
+            pageName: 'courses',
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
+
+exports.getCourse = async (req, res) => {
+    try {
+        const course = await Course.findOne({ slug: req.params.slug });
+        res.status(200).render('course-single', {
+            course,
+            pageName: 'courses',
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
