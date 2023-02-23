0e1ae0a353fb8876d9d91d0be823cab019708cd2
ertanuzunoglu <ertanuzunogl@gmail.com>
2023-02-23 13:53:10 +0300

    Lesson 06 : user model created

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,2 +1,3 @@
 exports.getIndexPage = (req, res) => res.status(200).render('index', { pageName: 'index' });
 exports.getAboutPage = (req, res) => res.status(200).render('about', { pageName: 'about' });
+exports.getRegisterPage = (req, res) => res.status(200).render('register', { pageName: 'register' });
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+    },
+});
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash;
+        next();
+    });
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
