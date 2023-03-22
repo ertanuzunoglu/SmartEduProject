597b8d669cae0c14d7c8eb8586b91321319e95ba
ertanuzunoglu <ertanuzunogl@gmail.com>
2023-03-22 17:50:08 +0300

    course-teacher relationship

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,15 @@
 const User = require('../models/User');
 const Category = require('../models/Category');
-const bcrypt = require('bcrypt');
+const Course = require("../models/Course");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
     try {
         const user = await User.create(req.body);
-        res.status(201).redirect('/login');
+        res.status(201).redirect("/login");
     } catch (error) {
         res.status(400).json({
-            status: 'fail',
+            status: "fail",
             error,
         });
         console.log(error);
@@ -23,13 +24,13 @@ exports.loginUser = async (req, res) => {
             bcrypt.compare(password, user.password, (err, same) => {
                 if (same) {
                     req.session.userID = user._id;
-                    res.status(200).redirect('/users/dashboard');
+                    res.status(200).redirect("/users/dashboard");
                 }
             });
         }
     } catch (error) {
         res.status(400).json({
-            status: 'fail',
+            status: "fail",
             error,
         });
         console.log(error);
@@ -38,15 +39,17 @@ exports.loginUser = async (req, res) => {
 
 exports.logoutUser = async (req, res) => {
     await req.session.destroy();
-    res.redirect('/');
+    res.redirect("/");
 };
 
 exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({ _id: req.session.userID });
     const categories = await Category.find();
-    res.status(200).render('dashboard', {
+    const courses = await Course.find({ user: req.session.userID });
+    res.status(200).render("dashboard", {
         user,
-        pageName: 'dashboard',
+        pageName: "dashboard",
         categories,
+        courses,
     });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,18 @@
 const Course = require('../models/Course');
-const Category = require('../models/Category');
+const Category = require("../models/Category");
 
 exports.createCourse = async (req, res) => {
     try {
-        const course = await Course.create(req.body);
-        res.status(201).redirect('/courses');
+        const course = await Course.create({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            user: req.session.userID,
+        });
+        res.status(201).redirect("/courses");
     } catch (error) {
         res.status(400).json({
-            status: 'fail',
+            status: "fail",
             error,
         });
     }
@@ -22,15 +27,15 @@ exports.getAllCourses = async (req, res) => {
             filter = { category: category._id };
         }
         const categories = await Category.find();
-        const courses = await Course.find(filter).sort('name');
-        res.status(200).render('courses', {
+        const courses = await Course.find(filter).sort("-createdAt");
+        res.status(200).render("courses", {
             courses,
             categories,
-            pageName: 'courses',
+            pageName: "courses",
         });
     } catch (error) {
         res.status(400).json({
-            status: 'fail',
+            status: "fail",
             error,
         });
     }
@@ -38,14 +43,14 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
     try {
-        const course = await Course.findOne({ slug: req.params.slug });
-        res.status(200).render('course-single', {
+        const course = await Course.findOne({ slug: req.params.slug }).populate("user");
+        res.status(200).render("course-single", {
             course,
-            pageName: 'courses',
+            pageName: "courses",
         });
     } catch (error) {
         res.status(400).json({
-            status: 'fail',
+            status: "fail",
             error,
         });
     }
